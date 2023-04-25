5af0d1d19f2a6016b2952948d952658a113690ed
jasonqzhang <jasonqzhang@tencent.com>
2023-04-25 18:45:26 +0800

    init

---
+++ routes/index.js
@@ -0,0 +1,62 @@
+const axios = require('axios')
+const router = require('koa-router')()
+
+const headers = {
+  'X-GitHub-Api-Version': '2022-11-28',
+  Accept: 'application/vnd.github+json',
+  Authorization: 'token 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',
+}
+
+router.get('/', async (ctx, next) => {
+  await ctx.render('index', {
+    title: 'Hello Koa 2!'
+  })
+})
+
+router.get('/string', async (ctx, next) => {
+  ctx.body = 'koa2 string'
+})
+
+router.get('/json', async (ctx, next) => {
+  ctx.body = {
+    title: 'koa2 json'
+  }
+})
+
+router.get('/:id', async (ctx, next) => {
+  const { id } = ctx.params
+  try {
+    const res = await axios({
+      method: 'GET',
+      url: 'https://api.github.com/repos/johnqiang/stable-diffusion/contents/' + id,
+      headers,
+    })
+    console.log('数据获取成功================', res.data)
+    let images = []
+    let config = ''
+    res.data.forEach(function(item) {
+      if (item.download_url.endsWith('json')) {
+        config = item.download_url
+      } else {
+        images.push(item.download_url)
+      }
+    })
+    console.log('========????', images, config)
+
+    const rst = await axios({
+      method: 'GET',
+      url: config,
+      headers,
+    })
+    console.log('=======res json', rst.data)
+    const { prompt, negativePrompt, samplingMethod, samplingSteps, CFG, Seed } = rst.data || {}
+    await ctx.render('index', {
+      title: 'Stable Diffusion Sharing', images, prompt, negativePrompt, samplingMethod, samplingSteps, CFG, Seed
+    })
+  } catch (err) {
+    console.log(err.message)
+  }
+
+})
+
+module.exports = router
